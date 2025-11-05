1a8aa6535d1d5b6a9b5d9753fbe68733c129834b
itsraulfraire <rfrairegaytan@gmail.com>
2025-11-05 14:36:40 -0600

    Refactor app.py and update database connection

--- app.py
+++ app.py
@@ -22,32 +22,10 @@ con_pool = mysql.connector.pooling.MySQLConnectionPool(
     pool_name="my_pool",
     pool_size=5,
     host="185.232.14.52",
-    database="u760464709_16005339_bd",
-    user="u760464709_16005339_usr",
-    password="/iJRzrJBz+P1"
+    database="u760464709_23005270_bd",
+    user="u760464709_23005270_usr",
+    password="$x[QjFu>Lt9H"
 )
-"""
-con_pool = mysql.connector.pooling.MySQLConnectionPool(
-    pool_name="my_pool",
-    pool_size=5,
-    host="localhost",
-    database="practicas",
-    user="root",
-    password="Test12345"
-)
-"""
-
-def pusherProductos():    
-    pusher_client = pusher.Pusher(
-        app_id="2046005",
-        key="12cb9c6b5319b2989000",
-        secret="7c193405c24182d96965",
-        cluster="us2",
-        ssl=True
-    )
-    
-    pusher_client.trigger("canalProductos", "eventoProductos", {"message": "Hola Mundo!"})
-    return make_response(jsonify({}))
 
 def login(fun):
     @wraps(fun)
@@ -62,12 +40,8 @@ def login(fun):
 
 @app.route("/")
 def landingPage():
-    return render_template("landing-page.html")
-
-@app.route("/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
-
+    return render_template("index.html")
+    
 @app.route("/login")
 def appLogin():
     return render_template("login.html")
@@ -89,10 +63,10 @@ def iniciarSesion():
     con    = con_pool.get_connection()
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT Id_Usuario, Nombre_Usuario, Tipo_Usuario
+    SELECT id_usuarios, nombre_usuario, tipo_usuario
     FROM usuarios
-    WHERE Nombre_Usuario = %s
-    AND Contrasena = %s
+    WHERE nombre_usuario = %s
+    AND contrasena = %s
     """
     val    = (usuario, contrasena)
 
@@ -109,8 +83,8 @@ def iniciarSesion():
     if registros:
         usuario = registros[0]
         session["login"]      = True
-        session["login-usr"]  = usuario["Nombre_Usuario"]
-        session["login-tipo"] = usuario["Tipo_Usuario"]
+        session["login-usr"]  = usuario["nombre_usuario"]
+        session["login-tipo"] = usuario["tipo_usuario"]
 
     return make_response(jsonify(registros))
 
@@ -130,186 +104,29 @@ def preferencias():
         "tipo": session.get("login-tipo", 2)
     }))
 
-@app.route("/productos")
-def productos():
-    return render_template("productos.html")
+@app.route("/playlists")
+@login_required
+def playlists():
+    return render_template("playlists.html")
 
-@app.route("/productos/buscar", methods=["GET"])
-@login
-def buscarProductos():
-    args     = request.args
-    busqueda = args["busqueda"]
-    busqueda = f"%{busqueda}%"
-    
-    try:
-        con    = con_pool.get_connection()
-        cursor = con.cursor(dictionary=True)
-        sql    = """
-        SELECT Id_Producto,
-            Nombre_Producto,
-            Precio,
-            Existencias
-        FROM productos
-        WHERE Nombre_Producto LIKE %s
-        OR    Precio          LIKE %s
-        OR    Existencias     LIKE %s
-        ORDER BY Id_Producto DESC
-        LIMIT 10 OFFSET 0
-        """
-        val    = (busqueda, busqueda, busqueda)
 
-        cursor.execute(sql, val)
-        registros = cursor.fetchall()
-
-    except mysql.connector.errors.ProgrammingError as error:
-        registros = []
-
-    finally:
-        if cursor:
-            cursor.close()
-        if con and con.is_connected():
-            con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/productos/categoria", methods=["GET"])
-@login
-def productosCategorias():
-    args      = request.args
-    categoria = args["categoria"]
-    
-    try:
-        con    = con_pool.get_connection()
-        cursor = con.cursor(dictionary=True)
-        sql    = """
-        SELECT Nombre_Producto
-        FROM productos
-        WHERE Categoria = %s
-        ORDER BY Nombre_Producto ASC
-        LIMIT 50 OFFSET 0
-        """
-        val    = (categoria, )
-
-        cursor.execute(sql, val)
-        registros = cursor.fetchall()
-
-    except mysql.connector.errors.ProgrammingError as error:
-        registros = []
-
-    finally:
-        if cursor:
-            cursor.close()
-        if con and con.is_connected():
-            con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/productos/ingredientes/<int:id>")
-@login
-def productosIngredientes(id):
-    con    = con_pool.get_connection()
+@app.route("/playlists/buscar")
+@login_required
+def buscarPlaylists():
+    db = DatabaseConnection.get_instance()
+    con = db.pool.get_connection()
     cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT productos.Nombre_Producto, ingredientes.*, productos_ingredientes.Cantidad FROM productos_ingredientes
-    INNER JOIN productos ON productos.Id_Producto = productos_ingredientes.Id_Producto
-    INNER JOIN ingredientes ON ingredientes.Id_Ingrediente = productos_ingredientes.Id_Ingrediente
-    WHERE productos_ingredientes.Id_Producto = %s
-    ORDER BY productos.Nombre_Producto
-    """
-    val    = (id,)
+    cursor.execute("""
+        SELECT idPlaylist, nombre, descripcion, url
+        FROM playlists
+        ORDER BY idPlaylist DESC
+        LIMIT 10
+    """)
+    data = cursor.fetchall()
+    cursor.close()
+    con.close()
+    return jsonify(data)
 
-    cursor.execute(sql, val)
-    registros = cursor.fetchall()
-    if cursor:
-        cursor.close()
-    if con and con.is_connected():
-        con.close()
 
-    return make_response(jsonify(registros))
-
-@app.route("/producto", methods=["POST"])
-@login
-def guardarProducto():
-    id     = request.form["id"]
-    nombre = request.form["nombre"]
-    precio = request.form["precio"]
-
-    existencias = request.form["existencias"]
-    if existencias == '':
-        existencias = None
-
-    # fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
-
-    con    = con_pool.get_connection()
-    cursor = con.cursor()
-
-    if id:
-        sql = """
-        UPDATE productos
-        SET Nombre_Producto = %s,
-            Precio          = %s,
-            Existencias     = %s
-        WHERE Id_Producto = %s
-        """
-        val = (nombre, precio, existencias, id)
-    else:
-        sql = """
-        INSERT INTO productos (Nombre_Producto, Precio, Existencias)
-        VALUES                (%s,              %s,     %s)
-        """
-        val =                 (nombre, precio, existencias)
-    
-    cursor.execute(sql, val)
-    con.commit()
-    if cursor:
-        cursor.close()
-    if con and con.is_connected():
-        con.close()
-
-    pusherProductos()
-
-    return make_response(jsonify({}))
-
-@app.route("/producto/<int:id>")
-@login
-def editarProducto(id):
-    con    = con_pool.get_connection()
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT Id_Producto, Nombre_Producto, Precio, Existencias
-    FROM productos
-    WHERE Id_Producto = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
-    registros = cursor.fetchall()
-    if cursor:
-        cursor.close()
-    if con and con.is_connected():
-        con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/producto/eliminar", methods=["POST"])
-def eliminarProducto():
-    id = request.form["id"]
-
-    con    = con_pool.get_connection()
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    DELETE FROM productos
-    WHERE Id_Producto = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
-    con.commit()
-    if cursor:
-        cursor.close()
-    if con and con.is_connected():
-        con.close()
-
-    pusherProductos()
-
-    return make_response(jsonify({}))
+if __name__ == "__main__":
+    app.run(debug=True)
